5d86a0cc86e4deff7cd952d53527e391b8e6d0c2
rakibul Islam <irakibul026@gmail.com>
2023-11-08 16:19:56 +0530

    new file:   README.txt 	new file:   css/bootstrap.min.css 	new file:   css/style.css 	new file:   css/tiny-slider.css 	new file:   images/bag.svg 	new file:   images/bowl-2.png 	new file:   images/bowl-3.png 	new file:   images/cart.svg 	new file:   images/couch.png 	new file:   images/cross.svg 	new file:   images/dots-green.svg 	new file:   images/dots-light.svg 	new file:   images/dots-yellow.svg 	new file:   images/envelope-outline.svg 	new file:   images/img-grid-1.jpg 	new file:   images/img-grid-2.jpg 	new file:   images/img-grid-3.jpg 	new file:   images/person-1.jpg 	new file:   images/person-1.png 	new file:   images/person_1.jpg 	new file:   images/person_2.jpg 	new file:   images/person_3.jpg 	new file:   images/person_4.jpg 	new file:   images/post-1.jpg 	new file:   images/post-2.jpg 	new file:   images/post-3.jpg 	new file:   images/product-1.png 	new file:   images/product-2.png 	new file:   images/product-3.png 	new file:   images/return.svg 	new file:   images/sofa.png 	new file:   images/support.svg 	new file:   images/truck.svg 	new file:   images/user.svg 	new file:   images/why-choose-us-img.jpg 	new file:   index.html 	new file:   js/bootstrap.bundle.min.js 	new file:   js/custom.js 	new file:   js/register.js 	new file:   js/tiny-slider.js 	new file:   pages/about.html 	new file:   pages/blog.html 	new file:   pages/cart.html 	new file:   pages/checkout.html 	new file:   pages/contact.html 	new file:   pages/login.html 	new file:   pages/services.html 	new file:   pages/shop.html 	new file:   pages/thankyou.html 	new file:   pages/user.html 	new file:   prepros-6.config 	new file:   public/entrepreneur-logo.png 	new file:   public/favicon.ico 	new file:   public/fortune-logo.png 	new file:   public/fortune-logo2.png 	new file:   public/mashable-logo.png 	new file:   public/microsoft-logo.png 	new file:   public/vercel-logo.png 	new file:   scss/style.scss

---
+++ js/register.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const regBtn = document.getElementById("reg-btn");
+	regBtn.addEventListener("click", () => {
+		console.log("hemmo");
+	});
+	const handleLogin = () => {
+		const fName = document.getElementById("fname");
+		const lName = document.getElementById("lname");
+		const email = document.getElementById("register-email");
+		const password = document.getElementById("password");
+		const confirmPassword = document.getElementById("confirm-password");
+	};
+});
