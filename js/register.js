6cc670074afc644990e67356a7a168c5d23a9518
rakibul Islam <irakibul026@gmail.com>
2023-11-09 12:39:21 +0530

    make single page for navigate easy

--- js/register.js
+++ js/register.js
@@ -2,12 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
 	const regBtn = document.getElementById("reg-btn");
 	regBtn.addEventListener("click", () => {
 		console.log("hello");
+		const handleLogin = () => {
+			const fName = document.getElementById("fname");
+			const lName = document.getElementById("lname");
+			const email = document.getElementById("register-email");
+			const password = document.getElementById("password");
+			const confirmPassword = document.getElementById("confirm-password");
+
+			if (fName && lName && email && password && confirmPassword) {
+				alert("Successfully registered !");
+			} else {
+				alert("All fields are mandatory !");
+			}
+		};
 	});
-	const handleLogin = () => {
-		const fName = document.getElementById("fname");
-		const lName = document.getElementById("lname");
-		const email = document.getElementById("register-email");
-		const password = document.getElementById("password");
-		const confirmPassword = document.getElementById("confirm-password");
-	};
 });
